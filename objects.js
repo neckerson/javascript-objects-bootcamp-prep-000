eb2145c4ee1c387de01e627a7a5c9f0662afc1c8
Nate Eckerson <nate.eckerson@gmail.com>
2018-05-27 18:52:53 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = {};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign(playlist, { [artistName]: songTitle })
+  //return Object.assign({}, playlist, { [artistName]: songTitle })
+  //return playlist[artistName]: songTitle })
 }
